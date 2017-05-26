2aff282b144333b69d7e0bf912832778cc753947
yuan yunlong <yuanyunlong@yuandeiMac.local>
2017-05-26 10:40:09 +0800

    change appmain.js

--- cpappmain.sh
+++ cpappmain.sh
@@ -1,1 +1,4 @@
 cp  /Users/yuanyunlong/Documents/iOS/HET/YYapp-h5-dev/household/wxbluetooth/static/js/appMain.js ./public/household/wxbluetooth/static/js/
+git add .
+git commit -m "change appmain.js"
+git push origin master 
